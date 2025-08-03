3b81b13b0690ae1af444ae62982bb12520c45fa5
Seremendy <rodrigomorello6@gmail.com>
2025-08-03 12:14:08 -0300

    Inicialização do projeto com as dependencias, PostgreSQL Drive e Spring Data JPA

---
+++ src/main/java/com/morello/LivrosNoBanco/LivrosNoBancoApplication.java
@@ -0,0 +1,13 @@
+package com.morello.LivrosNoBanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrosNoBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrosNoBancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morello/LivrosNoBanco/LivrosNoBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morello.LivrosNoBanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrosNoBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
